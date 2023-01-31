fe619f16bde8547e7e01da617c4a8ce0ac1a08dd
NotVerySalty-ps <123045167+NotVerySalty-ps@users.noreply.github.com>
2023-01-30 20:02:34 -0600

    Added some scripts

---
+++ FPS Game 1/Assets/Scripts/ThrowingObjects.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowingObjects : MonoBehaviour
+{
+    [Header("References")]
+    public Transform cam;
+    public Transform attackPoint;
+    public GameObject objectToThrow;
+
+    [Header("Settings")]
+    public int totalThrows;
+    public float throwCooldown;
+
+    [Header("Throwing")]
+    public KeyCode throwKey = KeyCode.Mouse0;
+    public float throwForce;
+    public float ThrowUpwardForce;
+
+    bool readyToThrow;
+
+    private void Start()
+    {
+        readyToThrow = true;
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(throwKey) && readyToThrow && totalThrows > 0)
+        {
+            Throw();
+        }
+    }
+    private void Throw()
+    {
+        readyToThrow = false;
+
+        GameObject projectile = Instantiate(objectToThrow, attackPoint.position, cam.rotation);
+
+        Rigidbody projectileRb = projectile.GetComponent<Rigidbody>();
+
+        Vector3 forceDirection = cam.transform.forward;
+
+        RaycastHit hit;
+
+        if(Physics.Raycast(cam.position, cam.forward, out hit, 500f))
+        {
+            forceDirection = (hit.point - attackPoint.position).normalized;
+        }
+
+        Vector3 forceToAdd = forceDirection * throwForce + transform.up * ThrowUpwardForce;
+
+        projectileRb.AddForce(forceToAdd, ForceMode.Impulse);
+
+        totalThrows--;
+
+        Invoke(nameof(ResetThrow), throwCooldown);
+    }
+
+    private void ResetThrow()
+    {
+        readyToThrow = true;
+    }
+}
